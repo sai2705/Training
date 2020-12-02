0d81bdda546c4b7e67f8725ac1dca1f68189b1b0
Saisriharsha Mankala <Saisriharsha.Mankala@neudesic.com>
2020-12-02 14:48:17 +0530

    Domain Event Grid Arm template added

--- RequestAuthentication/DeleteOrder/DeleteOrderDetails.cs
+++ RequestAuthentication/DeleteOrder/DeleteOrderDetails.cs
@@ -10,6 +10,7 @@ using System.Configuration;
 using System.Data.SqlClient;
 using System.Data;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace DeleteOrder
 {
@@ -18,27 +19,43 @@ namespace DeleteOrder
         [FunctionName("DeleteOrderDetails")]
         public static void Run([EventGridTrigger]EventGridEvent eventGridEvent, ILogger log)
         {
+            //Logging the Event Grid Data
             log.LogInformation(eventGridEvent.Data.ToString());
             var id = eventGridEvent.Data.ToString();
-            var identity = JsonConvert.SerializeObject(id);
+            
+            var parseid = JObject.Parse(id);
+            var parsevalue = parseid["id"].Value<string>();
+            var deserializevalue = JsonConvert.DeserializeObject(parsevalue);
+            
 
-        using  (  SqlConnection connection = new SqlConnection("Data Source = SMANKALA02; Initial Catalog = EventGrid; User ID = harsha; Password = Msai2705 *; Connection Timeout = 30; Integrated Security = True;"))
-                {
-                //var conn = ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString;
+        
+            //Sql Connection String 
+            using  (  SqlConnection connection = new SqlConnection("Data Source = SMANKALA02; Initial Catalog = EventGrid; User ID = harsha; Password = Msai2705*; Connection Timeout = 30;"))
+              
+            {
+                
 
                 try
                 {
                     
                     SqlCommand command = new SqlCommand();
                     command.Connection = connection;
+
+                    //Reading the stored procedure name
                     command.CommandText = "SP_Delete_Details";
+                    
+                    //Declaring the command type as stored procedure
+
                     command.CommandType = CommandType.StoredProcedure;
-                    //SqlParameter parameter = command.CreateParameter();
-                    //command.Parameters.Add(new SqlParameter("@ID",(eventGridEvent.Data.ToString()))).Value = id.Trim();
-                    command.Parameters.Add("@ID", SqlDbType.VarChar).Value = identity;
+                    //Passing the parameter value 
+                    SqlParameter parameter = command.CreateParameter();
+                    command.Parameters.Add("@ID", SqlDbType.VarChar).Value = deserializevalue;
+                  
+                  
                     connection.Open();
 
                     command.ExecuteNonQuery();
+                  
                 }
                 catch (Exception ex)
                 {
